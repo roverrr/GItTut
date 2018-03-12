17c17d998682183b69b4ee6714b49357f5723f62
Evgeny Smirnov <marsrover@macbook-pro-evgeny.local>
2018-03-12 15:55:06 +0300

    add map feature

---
+++ GItTut/MapForltinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForltinerary.swift
+//  GItTut
+//
+//  Created by Evgeny Smirnov on 12.03.2018.
+//  Copyright © 2018 Evgeny Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class MapForltinerary: NSObject {
+
+}
