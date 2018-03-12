de539992b732b9936650c97b08ab49fd070bc7e1
Evgeny Smirnov <marsrover@macbook-pro-evgeny.local>
2018-03-12 15:37:51 +0300

    Initial Commit

---
+++ GItTut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GItTut
+//
+//  Created by Evgeny Smirnov on 12.03.2018.
+//  Copyright © 2018 Evgeny Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
